9a83f7452446391feb63e17b7e6ce0c6b726e2f9
Vitor Pereira Resende <vitorpereiraresende26@gmail.com>
2024-07-21 00:28:26 -0300

    Adding data

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
 import pandas as pd
 import csv
 from datetime import datetime
+from data_entry import get_description, get_date, get_amount, get_category
 
 
 class CSV:
@@ -30,5 +31,16 @@ class CSV:
         print("Entry added to CSV file")
 
 
-CSV.initialize_csv()
-CSV.add_entry("20-07-2024", 102.3, "Income", "Salary")
+def add():
+    CSV.initialize_csv()
+    date = get_date(
+        "Enter the date of the transaction (dd-mm-yyyy) or enter for today's date: ",
+        allow_default=True
+    )
+    amount = get_amount()
+    category = get_category()
+    description = get_description()
+    CSV.add_entry(date, amount, category, description)
+
+
+add()
